1544148243cf363d2957eee805c90cb2d730e79a
Ivo Petrônio <156373389+ivo-petronio@users.noreply.github.com>
2024-10-30 12:07:01 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@
 function completeTask(task) {
 	task.classList.toggle("completed");
 }
+
+async function deleteTask(task) {
+	await doDelete(li);
+	task.parentNode.remove();
+}
+
+function doDelete(li) {
+	setInterval(() => {
+		li.style.transform = "rotateY(630deg)";
+	}, 2200);
+}
