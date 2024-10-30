6e76c63ba93777c842674e96aca50dfd8e595262
Cyber_Petronio <kawaiivo@gmail.com>
2024-10-29 22:34:39 -0300

    animações

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@
 function completeTask(task) {
 	task.classList.toggle("completed");
 }
+
+async function deleteTask(li) {
+	li.parentNode.style.transform = "perspective(13cm) rotateY(630deg)";
+	await deleteAnimation(li);
+}
+
+function deleteAnimation(li) {
+	return (new Promise((resolve) => {
+		setTimeout(() => {
+			li.parentNode.remove();
+		}, 2200);
+	}))
+	
+}
